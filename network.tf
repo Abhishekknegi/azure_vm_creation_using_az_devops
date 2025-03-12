aef14cdcd35d62a035c9dedaabd9d86b89d7df1a
Abhishekknegi <42269049+Abhishekknegi@users.noreply.github.com>
2025-03-12 22:12:17 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,19 +4,53 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "UK South"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-vnet"
-  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
-  address_space       = ["172.16.0.0/16"]
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["172.16.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "example" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = "Standard_B1ls"  # Free-tier eligible SKU
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "freevm"
+    admin_username = "azureuser"
+    admin_password = "SecureP@ssw0rd!"  # Avoid hardcoding passwords
+  }
+
+ 
